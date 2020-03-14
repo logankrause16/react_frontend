0a3aa70d40467490a6fc1e36887528ddcefee4d4
Logan Krause <logankrause16@gmail.com>
2020-03-14 16:42:24 -0400

    Finally getting the map to work. I can see my house +1

--- src/features/map/Map.js
+++ src/features/map/Map.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Viewer, Entity } from 'resium';
 import { hot } from 'react-hot-loader/root';
+import { Viewer, Entity } from 'resium';
 import { Cartesian3 } from 'cesium';
 
 function Map() {
 
-    render() {
-        return (
-            <Viewer full>
-                <Entity position={Cartesian3.fromDegrees(0, 0, 0)} point={ 0, auto} />
-            </Viewer>
-        );
-    }
+    return (
+        <Viewer full>
+            <Entity position={Cartesian3.fromDegrees(0, 0, 0)} point={{pixelSize: 20}} />
+        </Viewer>
+    );
 
 }
+
+export default hot(Map);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
-<<<<<<< HEAD
-import './index.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-=======
->>>>>>> 0baa6c14306f37840436575417042e2b523252a9
 import DataGrid from './features/datagrid/Datagrid';
+import Map from './features/map/Map';
 
 ReactDOM.render(
-  <Router>
+  <Router history={createBrowserHistory()}>
     <Switch>
-<<<<<<< HEAD
-      <Route exact path='/' component={ App } />
-      <Route path='/datagrid' component={ DataGrid } />
-=======
       <Route exact path='/' component={App}></Route>
       <Route path='/datagrid' component={ DataGrid }></Route>
->>>>>>> 0baa6c14306f37840436575417042e2b523252a9
+      <Route path='/map' component={ Map }></Route>
     </Switch>
   </Router>,
   document.getElementById('root')
